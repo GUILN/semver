1bf82c29a904d367e178619d889cd661a2227905
guiln <guilherme.l.nunes@gmail.com>
2022-11-13 23:35:16 -0300

    feat: finished implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Semantic Version tools
+
+### To compile
+
+Run: 
+```bash
+make release
+```
+
+Use binary named `getver`.
+
+### To use
+
+```bash
+getver --current-version v2.1.4 --comment "feat: this is a feature"
+# Output:
+v2.2.0
+```
--- cli/src/getver/bin/main.rs
+++ cli/src/getver/bin/main.rs
@@ -1,16 +1,29 @@
+use core::calculate_version;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// Current Version
+    /// #Example:
+    /// v2.3.5
     #[clap(short, long, value_parser)]
-    version: String,
+    current_version: String,
+    /// Semantic Version Comment
+    /// 
+    /// # Example:
+    /// feat: this is a feature.
+    #[clap(short, long, value_parser)]
+    comment: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    println!("Hello from get ver!");
+    let new_version = calculate_version(args.current_version.as_str(), args.comment.as_str().try_into()?)?;
+
+    println!("{}", new_version);
 
     Ok(())
 }
